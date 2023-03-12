e7b017e02ff0b4157b7b7492a76fdc0a54b92c34
MOHAMED RIASKHAN <mohamedriaskhan@gmail.com>
2023-03-12 21:44:18 +0530

    UC1 cases

---
+++ ProductReview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementDay35
+{
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+        public int Rating { get; set; }
+        public override string ToString()
+        {
+            return $"ProductId:{ProductId} UserId: {UserId} Review: {Review} IsLike: {IsLike} Rating: {Rating}";
+        }
+    }
+}
