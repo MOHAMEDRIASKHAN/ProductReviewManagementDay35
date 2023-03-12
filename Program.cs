de217a02d6982d456c0fb1d4fe24c10373d3da41
MOHAMED RIASKHAN <mohamedriaskhan@gmail.com>
2023-03-12 21:38:21 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewManagementDay35
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Product Review Management");
+        }
+    }
+}
